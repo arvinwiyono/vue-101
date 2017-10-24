9b755bc2118232b059aaeccf657be3c12b5f363b
Arvin Wiyono <arvinwiyono@gmail.com>
2017-10-25 00:31:44 +1100

    Wrap component inside a component

--- laracasts/js/main.js
+++ laracasts/js/main.js
@@ -2,6 +2,25 @@ Vue.component('task', {
 	template: '<li><slot></slot></li>'
 })
 
+Vue.component('task-list', {
+	template: `
+	<div>
+		<task v-for="task in tasks">{{task.description}}</task>
+	</div>`,
+	
+	data(){
+		return {
+			tasks: [
+				{ description: 'Go to the store', completed: true },
+				{ description: 'Finish tutorial', completed: false },
+				{ description: 'Finish thesis', completed: false },
+				{ description: 'Read emails', completed: true },
+				{ description: 'Clean room', completed: false }
+			]
+		}
+	}
+})
+
 new Vue({
 	el: '#root'
 });
