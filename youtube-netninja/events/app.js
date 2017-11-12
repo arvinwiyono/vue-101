1fdaf00c79dc6a60b864e606613a0e5a5662964d
Arvin Wiyono <arvinwiyono@gmail.com>
2017-11-12 14:44:42 +0700

    Events

---
+++ youtube-netninja/events/app.js
@@ -0,0 +1,25 @@
+let app = new Vue({
+	el: 'div#vue-app',
+	data: {
+		age: 23,
+		x: 0,
+		y: 0
+	},
+	methods: {
+		incrementAge: function(year=1){
+			this.age += year;
+		},
+		decrementAge: function(year=1){
+			if(this.age - year >= 0){
+				this.age -= year;
+			}
+			else{
+				alert(`Cannot subtract ${year} year(s) from age!`);
+			}
+		},
+		updateXY: function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		}
+	}
+});
