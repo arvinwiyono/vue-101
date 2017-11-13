717614e394630989be3eaa7e9a20434b8259aae9
Arvin Wiyono <arvinwiyono@gmail.com>
2017-11-13 12:10:05 +0700

    Empty vue app generated with vue-cli

---
+++ youtube-netninja/vuejs-playlist/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="title">{{title}}</h1>
+    <p class="subtitle">{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: 'Your first Vue file, whooo!'
+    }
+  },
+  methods:{
+    greeting: function(){
+      return 'Heyyyyy cowboy.'
+    }
+  }
+}
+</script>
+
+<style>
+  p.subtitle{
+    color: green;
+  }
+</style>
